e5febd2e4a9bedae4459f883e22749453a10058a
Jonathan Wilson <jon.wilsond@gmail.com>
2018-06-29 12:12:51 -0400

    added lists

--- ToDoList/Models/List.cs
+++ ToDoList/Models/List.cs
@@ -5,5 +5,14 @@ using System.Web;
 
 namespace ToDoList.Models {
 	public class List {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string UserId { get; set; }
+		public virtual User User { get; set; }
+		public bool Completed { get; set; } = false;
+
+		public List() {
+
+		}
 	}
 }
--- ToDoList/Models/User.cs
+++ ToDoList/Models/User.cs
@@ -5,5 +5,11 @@ using System.Web;
 
 namespace ToDoList.Models {
 	public class User {
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public User() {
+
+		}
 	}
 }
